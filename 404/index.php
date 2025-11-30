56c00efcd76eee60e0a7a09b573bfce8ec2ecb56
Kievsky332 <falileevry@gmail.com>
2025-11-30 17:55:22 +0300

    Fixed 404!

---
+++ 404/index.php
@@ -0,0 +1,16 @@
+<?php
+            require_once "../partials/base.html";         
+?>
+<link rel="stylesheet" href="../css/style1.css">
+<body>
+    <div id="main">
+        <?php
+            require_once "../partials/header.html";
+        ?>
+        <center><p>404 ошибка !Страница не найдена .<a href="/">Назад.</a></center>
+        </p>
+    </div>
+    <?php
+        require_once "../partials/footer.html";
+    ?>
+</body>
