54cf80cc04ba5162dd9b6ae1a5aa41386a09ad13
Kievsky332 <falileevry@gmail.com>
2025-11-30 14:12:09 +0300

    base.html

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+            require_once "partials/base.html";
+            
+?>
+<link rel="stylesheet" href="css/style1.css">
+<body>
+    <div id="main">
+        <?php
+            require_once "partials/header.html";
+        ?>
+        <center><img id="imglogo" src="https://i.pinimg.com/236x/c8/cc/24/c8cc24bba37a25c009647b8875aae0e3.jpg" ></center><br>
+        <center><input type="button"value="Рассказать эмоции"> </center><br>
+        <div id="HI" class="divi">
+            <center><h2>За сегодня:</h2></center>
+            <div>
+                <img class="menshe" src="https://emoji.aranja.com/emojis/apple/1f643.png">
+                <p class="bolshe">12323</p>
+            </div><br>
+            <div>
+                <img class="menshe"  src="https://uprostim.com/wp-content/uploads/2021/04/image084-3.png">
+                <p class="bolshe">12323</p>
+            </div>
+            <center><h3>Последнаяя реакция:</h3></center>
+            <img class="menshe" src="https://uprostim.com/wp-content/uploads/2021/04/image084-3.png">
+        </div>
+        <div id="right11" class="divi">
+            <img src="image.png" class="menshe">
+           <center> <p class="bolshe">12379</p></center>
+        </div>
+    </div>
+    <?php
+        require_once "partials/header.html";
+    ?>
