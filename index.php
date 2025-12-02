9bbf6a29835bb124f961b7f0cdca8b8b2a89c999
Kievsky332 <falileevry@gmail.com>
2025-12-02 22:18:54 +0300

    add neg/poz/last

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-            require_once "partials/base.html";
-               
+            $data = include 'login-php/last_emoji.php'; 
+            require_once "partials/base.html";            
 ?>
+
 <link rel="stylesheet" href="css/style1.css">
 <body>
     
@@ -9,20 +10,32 @@
         <?php
             require_once "partials/header.html";
         ?>
-        <center><input type="image" id="imglogo" src="https://cdn-icons-png.flaticon.com/512/10942/10942081.png" value="aw" onclick="awu()"></center><br>
-        <center><input type="button" value="Рассказать эмоции"> </center><br>
+        <center><input type="image"     id="imglogo" src="https://cdn-icons-png.flaticon.com/512/10942/10942081.png" onclick="awu()"></center><br>
+        <form action="../login-php/emoji-send.php" method="post" >
+             <input type="hidden" id="emoter" name="emote" value="1">        
+            <center><input type="submit" id="pon" value="Рассказать эмоции"> </center><br>
+        </form>
+
         <div id="HI" class="divi">
-            <center><h2>За сегодня:</h2></center>
+            <center><h2>Всего:</h2></center>
             <div>
-                <img class="menshe" src="https://emoji.aranja.com/emojis/apple/1f643.png">
-                <p class="bolshe">12323</p>
+                <img class="menshe" src="https://cdn-icons-png.flaticon.com/512/10942/10942081.png">
+                <p class="bolshe"><?php echo htmlspecialchars($data['pozy']); ?></p>
             </div><br>
             <div>
-                <img class="menshe"  src="https://uprostim.com/wp-content/uploads/2021/04/image084-3.png">
-                <p class="bolshe">12323</p>
+                <img class="menshe"  src="https://cdn-icons-png.flaticon.com/512/12657/12657875.png">
+                <p class="bolshe"><?php echo htmlspecialchars($data['negativ']); ?></p>
             </div>
             <center><h3>Последнаяя реакция:</h3></center>
-            <img class="menshe" src="https://uprostim.com/wp-content/uploads/2021/04/image084-3.png">
+            <img class="menshe" 
+    <?php 
+    if ($data['last'] == 0) {
+        echo "src='https://cdn-icons-png.flaticon.com/512/12657/12657875.png'";
+    } else {
+        echo "src='https://cdn-icons-png.flaticon.com/512/10942/10942081.png'";
+    } 
+    ?>
+>
         </div>
         <div id="right11" class="divi">
             <img src="image.png" class="menshe">
--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 function awu() {
     let a = document.getElementById("imglogo");
-    if (a.value === "aw") {
-        a.value = "sad";
+    let b = document.getElementById("emoter");
+    if (b.value === "1") {
+        b.value = "0";
         a.src = "https://cdn-icons-png.flaticon.com/512/12657/12657875.png" 
-    } else if (a.value === "sad") {
-        a.value = "aw";
+    } else if (b.value === "0") {
+        b.value = "1";
         a.src = "https://cdn-icons-png.flaticon.com/512/10942/10942081.png"
     } else {
         console.log("error");
